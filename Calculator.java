90b4347f613b43a8e6efd51356bbe0c3dc7830c8
sukhibhatti <bhattisukhdeep69@gmail.com>
2024-08-05 17:34:41 +0530

    First

---
+++ Calculator.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Calculator {
+   
+    public static void main(String args[]){
+ try{
+         Scanner sc= new Scanner(System.in);
+         double num1,num2;
+         System.out.println("Enter first number--");
+         num1=sc.nextDouble();
+         System.out.println("Enter Second number--");
+         num2=sc.nextDouble();
+        
+         System.out.println("Select operator--");
+         char op= sc.next().charAt(0);
+         double r=0;
+         
+
+        switch(op){
+                       case '+':
+                       r=num1+num2;
+                       break;
+
+                        case '-':
+                       r=num1-num2;
+                       break;
+
+                       case '*':
+                       r=num1*num2;
+                       break;
+
+                       case '/':
+                       r=num1/num2;
+                       break;
+                       
+                       default:
+                       System.out.println("***Enter a valid operator***");
+
+        }
+                  System.out.println("The result is:");
+                  System.out.println(num1+" "+op+" "+num2+" = "+r);
+
+    }
+
+            catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+}
+}
+
+
+
+ 
+
+    
+
---
+++ Variables.java
@@ -0,0 +1,27 @@
+ class Variables {
+
+    static int y=90;
+    int z=34;
+
+    public int addNum(){
+        int a=23;
+        int b=34;
+        int c;
+        c=a+b;
+        System.out.println(a);
+        
+        return a;}
+        public static void main(String args[]){
+          
+            System.out.println(y);
+            Variables v3=new Variables();
+            System.out.println(v3.z);
+     
+    
+       
+
+            
+        
+
+    }
+ }
